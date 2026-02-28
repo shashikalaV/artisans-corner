eac708a4cd78fbca729bc60d0a0e882571f46666
Shashikala <sonuvgowda05@gmail.com>
2026-02-28 15:18:52 +0530

    Initial Commit - Vite + React frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="container text-center mt-5">
+      <h1 className="text-purple">Artisan's Corner</h1>
+      <button className="btn btn-purple mt-3">
+        Explore Products
+      </button>
+    </div>
+  )
+}
+
+export default App
